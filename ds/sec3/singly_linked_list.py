cd2fd5db577eb176dbe0914ca73a2c890612ad9e
Ravi Ranjan <raviranjan@vmware.com>
2019-05-19 22:48:52 +0530

    Adding stack and linked list implementation

---
+++ ds/sec3/singly_linked_list.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_node(self, key):
+
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+
+    cur_node = None
+
+
+llist = LinkedList()
+llist.append('A')
+llist.append('B')
+llist.append('C')
+llist.append('D')
+llist.delete_node('B')
+llist.prepend('E')
+llist.print_list()
--- ds/sec3/stack.py
+++ ds/sec3/stack.py
@@ -1,4 +1,44 @@
-# size
-# pop
-# push
-# top
+"""
+Stack data Structure
+D
+C
+B
+A
+"""
+
+
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def is_empty(self):
+        return self.items == []
+
+    def get_stack(self):
+        return self.items
+
+    def top(self):
+        return self.items[-1]
+
+    def get_size(self):
+        return len(self.items)
+
+
+s = Stack()
+s.push('A')
+s.push('B')
+s.pop()
+s.push('C')
+print(s.get_size())
+s.push('D')
+print(s.get_stack())
